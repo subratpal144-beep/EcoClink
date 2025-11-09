607eefab7c5f385e73775d8ec35df2f723af98c4
subratpal144-beep <subratpal144@gmail.com>
2025-11-09 14:02:03 +0530

    Update About.tsx

--- EcoStudentVenture/client/src/components/About.tsx
+++ EcoStudentVenture/client/src/components/About.tsx
@@ -8,7 +8,7 @@ const teamMembers = [
   { name: "Shadiya Khan", initials: "SK" },
   { name: "Prachi", initials: "P" },
   { name: "Saloni", initials: "S" },
-  { name: "Khushi", initials: "K" },
+  { name: "Khushi", initials: "KH" },
 ];
 
 export default function About() {
